740a82b404853b44f5462010141465a1f7be50da
Owinnie <tillawinnie@gmail.com>
2022-06-05 10:24:39 -0700

    Tech interview

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+  * is_palindrome - check list if palindr
+  * @head: first node
+  * Return: 0 - !palindr, else 1
+  */
+int is_palindrome(listint_t **head)
+{
+	return (0)
+}
