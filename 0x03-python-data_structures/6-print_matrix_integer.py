3c81e21d72a4cf7846e4114d6837e1e5de23f37b
Owinnie <tillawinnie@gmail.com>
2022-06-05 08:27:16 -0700

    6,

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for ls in matrix:
         for ele_i in range(len(ls)):
-            print("{:d}".format(ls[ele_i]), end="" if ele_i == len(ls) - 1 else " ")
+            print("{:d}".format(ls[ele_i]), 
+                end="" if ele_i == len(ls) - 1 else " ")
         print("")
