10702a7d4ff129d6972ec8b83ae222c36e9b3f40
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-06-23 20:07:46 +0300

    1.

--- 0x0711-python-test_driven_development/2-matrix_divided.py
+++ 0x0711-python-test_driven_development/2-matrix_divided.py
@@ -8,9 +8,11 @@ that divides all elements of a matrix
 
 def matrix_divided(matrix, div):
     """Divide all elements of matrix by div
+
     Args:
         matrix (list): list of lists
         div (int): divisor
+
     Returns:
         list: new matrix divided by div
     """
@@ -18,7 +20,7 @@ def matrix_divided(matrix, div):
         raise TypeError(
             "matrix must be a matrix (list of lists)"
             " of integers/floats"
-            )
+        )
     i = 1
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
@@ -27,20 +29,20 @@ def matrix_divided(matrix, div):
     for rw in matrix:
         if type(rw) != list:
             raise TypeError(
-            "matrix must be a matrix (list of lists)"
-            " of integers/floats"
+                "matrix must be a matrix (list of lists)"
+                " of integers/floats"
             )
         for el in rw:
             if type(el) not in [int, float]:
                 raise TypeError(
-            "matrix must be a matrix (list of lists)"
-            " of integers/floats"
-            )
+                    "matrix must be a matrix (list of lists)"
+                    " of integers/floats"
+                )
         while i < len(matrix):
             if len(rw) != len(matrix[i]):
                 raise TypeError(
                     "Each row of the matrix"
                     " must have the same size"
-                    )
+                )
             i += 1
     return [[round(el / div, 2) for el in rw] for rw in matrix]
