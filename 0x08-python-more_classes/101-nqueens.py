1ce1f8e8b120ef543c1651d809b48d350ad6f6cf
Owinnie <tillawinnie@gmail.com>
2022-06-27 15:05:59 -0700

    10.

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Program that solves the N queens problem
+The N queens puzzle is the challenge of
+placing N non-attacking queens on an
+N×N chessboard"""
+
+
+class NQueens:
