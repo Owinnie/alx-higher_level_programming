fa7ac17f3dd5683606d49803d2784f7d9b747db3
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-05 04:40:44 +0300

    9.

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,25 +14,25 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Check if of type int and if > than 0"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
     """Rectangle extends BaseGeometry"""
     def __init__(self, width, height):
-        """Initialize"""
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
-    def area(self, width, height):
-        """Find / return area"""
-        return (self.__width * self.__height)
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
 
-    def __str___(self):
-        """Prints informal str"""
+    def __str__(self):
+        """informal string representation of the rectangle"""
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
