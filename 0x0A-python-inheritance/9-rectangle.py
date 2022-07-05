acbcb2209901854266c47270205f4d5503b1bea9
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-05 04:32:30 +0300

    9.

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,8 +3,21 @@
 """Write a class Rectangle
 that inherits BaseGeometry"""
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+class BaseGeometry:
+    """defines public instance method
+    called area()"""
+    def area(self):
+        """Finds area else raises
+        exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Check if of type int and if > than 0"""
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Rectangle extends BaseGeometry"""
