a7fcdee92366656a32569fb75f0dc9e9e788a095
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-06 06:18:23 +0300

    0.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""read file"""
+"""Write a function that reads a
+text file (UTF8) and prints it to stdout"""
 
 
 def read_file(filename=""):
-    """read file"""
+    """Reading"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
