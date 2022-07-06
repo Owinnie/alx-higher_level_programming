3a6a567a5fe6fe7984d68b902e04927544e302fe
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-06 05:58:33 +0300

    2.

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,6 +9,7 @@ import json
 
 
 def append_write(filename="", text=""):
-    """Append"""
+    """Append a string at the end of a text file
+    and returns the number of characters added"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
