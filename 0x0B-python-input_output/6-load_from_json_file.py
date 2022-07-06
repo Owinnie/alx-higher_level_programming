707265e0fffde5b96819da9c02712100cf748893
Owinnie <tillawinnie@gmail.com>
2022-07-05 19:19:11 -0700

    5, 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Write a function that creates
+an Object from a “JSON file”"""
+
+
+def load_from_json_file(filename):
+    """Load from json"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
