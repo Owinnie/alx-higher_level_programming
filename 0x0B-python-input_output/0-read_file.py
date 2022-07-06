40248a59d5b60bd478925d3980a419d65f0db9a6
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-06 05:50:37 +0300

    0.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ text file and prints it to stdout"""
 
 def read_file(filename=""):
     """Reading"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
