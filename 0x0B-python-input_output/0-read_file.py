31cc33e5964f2c7ded74086a2faaca87b84a0731
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-06 06:03:08 +0300

    0.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""reading file"""
+"""defining read_file function"""
 
 
 def read_file(filename=""):
-    """reading file"""
-    with open(filename, encoding="utf-8") as f:
+    """reads filename with utf-8"""
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
