8caaa762d01af06dfa0a92b8bde1b2c25f04980d
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-11 22:44:43 +0300

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """informal str representation"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}"
+        return ("[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"
                 .format(self.id, self.__x, self.__y,
                         self.__width, self.__height))
 
