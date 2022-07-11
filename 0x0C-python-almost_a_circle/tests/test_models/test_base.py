4c519521a70580dc8ad38c4879d253097412cdad
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-11 22:57:29 +0300

    base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,44 +8,7 @@ from models import base
 from models import rectangle
 Base = base.Base
 Rectangle = rectangle.Rectangle
-import inspect
-import pep8
 import json
-
-
-class TestDocs(unittest.TestCase):
-    """Tests to check the documentation and style of Base class"""
-    @classmethod
-    def setClass(cls):
-        """Set up doc tests"""
-        cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
-
-    def test_pep8_conformance_base(self):
-        """Test that base.py conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_base(self):
-        """Test that test_base.py conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_module_docstr(self):
-        """Test module docstr"""
-        self.assertTrue(len(base.__doc__) >= 1)
-
-    def test_class_docstr(self):
-        """Test Base class docstr"""
-        self.assertTrue(len(Base.__doc__) >= 1)
-
-    def test_func_docstr(self):
-        """Test all functions' docstr"""
-        for func in self.base_funcs:
-            self.assertTrue(len(func[1].__doc__) >= 1)
 
 
 class TestBaseClass(unittest.TestCase):
