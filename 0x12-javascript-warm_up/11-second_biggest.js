40387f5c308a5113073d705c70a9e7f44f41b119
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-06 04:58:21 +0300

    11.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-let secBig = 0;
-const args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  secBig = args[args.length - 2];
+let argv = process.argv;
+
+if (argv.length < 4) { console.log(0); } else {
+  argv = argv.slice(2);
+  let nums = argv.map(x => parseInt(x));
+  nums = nums.sort(function (a, b) { return a - b; });
+  console.log(nums[nums.length - 2]);
 }
-console.log(secBig);
