036d1f8f27facf1e21a5864be3f250ff76e5fd3d
Owinnie <tillawinnie@gmail.com>
2022-09-23 12:00:09 -0700

    1.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the
-URL and displays the value of the X-Request-Id
-found in the header of the response"""
+"""Takes URL, sends request, displays value X-Request-Id."""
 
 
 from sys import argv
