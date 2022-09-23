e6c2d58564a7a720a55a1975e83a4981dcbe47fc
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-23 03:42:35 +0300

    technical interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,4 +3,15 @@
 
 def find_peak(list_of_integers):
     """I find the peak"""
-    ...
+    if A == []:
+        return None
+    def recursive(A, left=0, right=len(A) - 1):
+        """recursive function"""
+        mid = (left + right) // 2
+        if ((mid == 0 or A[mid - 1] <= A[mid]) and
+                (mid == len(A) - 1 or A[mid + 1] <= A[mid])):
+            return A[mid]
+        if mid - 1 >= 0 and A[mid - 1] > A[mid]:
+            return recursive(A, left, mid - 1)
+        return recursive(A, mid + 1, right)
+    return recursive(A)
