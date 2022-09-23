7f71284829aa8b203041ad2c795bffe681d3d7cd
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-23 23:42:59 +0300

    9. #adv

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,6 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     username = argv[1]
     password = argv[2]
-    ath = HTTPBasicAuth(username, password)
-    body = requests.get("https://api.github.com/user", ath)
+    auth = HTTPBasicAuth(username, password)
+    body = requests.get("https://api.github.com/user", auth=auth)
     print(body.json().get("id"))
