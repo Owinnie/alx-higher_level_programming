23acb919d300375d66f3a6e26a99b52b87677216
Owinnie <tillawinnie@gmail.com>
2022-09-23 11:38:21 -0700

    0.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print("-\t type: ", type(body))
-        print("-\t content: ", body)
-        print("-\t utf8 content: ", body.decode("utf-8"))
+        print("-\t type:", type(body))
+        print("-\t content:", body)
+        print("-\t utf8 content:", body.decode("utf-8"))
