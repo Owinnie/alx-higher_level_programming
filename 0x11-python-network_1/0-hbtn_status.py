bebd6950e527ae6031985ef90164b493b84e35d5
Owinnie <tillawinnie@gmail.com>
2022-09-23 11:31:27 -0700

    0.

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Fetch url: https://alx-intranet.hbtn.io/status"""
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type: ", type(body))
+        print("    - content: ", body)
+        print("    - utf8 content: ", body.decode("utf-8"))
