243f39b8a9a0bc9d2645538621b1a5db439d1b05
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-23 23:53:52 +0300

    #adv: backend interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,7 +2,18 @@
 """10. Time for an interview!"""
 
 
-import sys
+from sys import argv
 import requests
 
+if __name__ == "__main__":
+    # Arguments--> arg1: repo name, arg2: repo owner
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            argv[2], argv[1])
 
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
+    except IndexError:
+        pass
