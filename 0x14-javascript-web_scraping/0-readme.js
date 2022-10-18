91e7063b271126d18bacc6e8ccd41ea4650c0b7f
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-18 17:35:13 +0300

    0.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
+const req = require('req');
 
+req.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
